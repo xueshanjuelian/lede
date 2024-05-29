1fffd1374f0338aee529c5532aec55101f026f54
xueshanjuelian <xueshanjuelian@163.com>
2024-05-29 10:58:27 +0800

    Update mt7621.mk

--- target/linux/ramips/image/mt7621.mk
+++ target/linux/ramips/image/mt7621.mk
@@ -545,7 +545,7 @@ TARGET_DEVICES += dlink_dir-882-r1
 
 define Device/d-team_newifi-d2
   $(Device/uimage-lzma-loader)
-  IMAGE_SIZE := 32448k
+  IMAGE_SIZE := 64896k
   DEVICE_VENDOR := Newifi
   DEVICE_MODEL := D2
   DEVICE_COMPAT_VERSION := 1.1
@@ -556,7 +556,7 @@ TARGET_DEVICES += d-team_newifi-d2
 
 define Device/d-team_pbr-m1
   $(Device/dsa-migration)
-  IMAGE_SIZE := 32448k
+  IMAGE_SIZE := 64896k
   DEVICE_VENDOR := PandoraBox
   DEVICE_MODEL := PBR-M1
   DEVICE_PACKAGES := kmod-ata-ahci kmod-mt7603 kmod-mt76x2 kmod-sdhci-mt7620 \
